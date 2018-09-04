5f9ef2cb0631101e3882b9dded26dd5b85f522a8
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-09-04 15:01:15 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear() - 1
+  }
+}
+
+
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+    this.distance = this.blocksTravelled(beginningLocation, endingLocation)
+}
+  blocksTravelled() {
+  let startingVertical = parseInt(this.beginningLocation.vertical)
+  let endingVertical = parseInt(this.endingLocation.vertical)
+  let verticalDistance = Math.abs(startingVertical - endingVertical)
+
+  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+
+  let startingHorizontal = eastWest.indexOf(this.beginningLocation.horizontal)
+  let endingHorizontal = eastWest.indexOf(this.endingLocation.horizontal)
+  let horizontalDistance = Math.abs(startingHorizontal - endingHorizontal)
+
+  return verticalDistance + horizontalDistance
+
+}
+
+estimatedTime(peak) {
+
+  if (peak === true) {
+    return this.distance / 2 }
+  else {
+    return this.distance / 3 }
+}
+
+
+}
