ead28242c053d0642dc5d2a8d401fc28a0911310
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-09-04 09:56:37 +0100

    Jo

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
+class Driver {
 
+  constructor(name, startDate) {
+      this.name = name;
+      this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return (year - this.startDate.getFullYear())
+  }
+
+
+}
+
+const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+     let vertTravel = this.endingLocation.vertical - this.beginningLocation.vertical;
+
+     //get the index no. of start avenue
+     let startArray
+      for(const index in eastWest) {
+        if (eastWest[index] === this.beginningLocation.horizontal) {
+          startArray = index
+        }
+      }
+      //get the index no. of end avenue
+     let endArray
+      for(const index in eastWest) {
+        if (eastWest[index] === this.endingLocation.horizontal) {
+          endArray = index
+        }
+      }
+
+    let horizTravel = endArray - startArray
+    return vertTravel + horizTravel
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return (this.blocksTravelled() / 2)
+    } else {
+      return (this.blocksTravelled() / 3)
+    }
+  }
+
+}
